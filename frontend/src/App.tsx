ff19660798f8ae028703ec14b4b9c201225d296c
katelyn chi <katelynchi@crc-dot1x-nat-10-239-156-58.bu.edu>
2024-04-05 19:36:48 -0400

    adding spotify login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import "./App.css";
 import { HomePage } from "./components/HomePage/HomePage";
 
+
 function App() {
   if (process.env.NODE_ENV === "development") {
     axios.defaults.baseURL = "http://localhost:3000";
@@ -13,7 +14,32 @@ function App() {
   }
 
   return (
+    
     <>
+
+         <HomePage />
+
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+
       <HomePage />
 
       {/* <div>
@@ -37,6 +63,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
     </>
+
+
   );
 }
 
