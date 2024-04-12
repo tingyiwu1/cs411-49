7716b59c0c58add09b160c97a4fcbac74c4e30bb
TingYi Wu <tingyi.wu1@gmail.com>
2024-04-12 09:58:09 -0400

    backend spotify oauth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 import fastify, { RouteShorthandOptions } from "fastify";
 import cors from "@fastify/cors";
-import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import cookie from "@fastify/cookie";
+import oauthPlugin from "@fastify/oauth2";
+import { SpotifyApi, AccessToken } from "@spotify/web-api-ts-sdk";
 import OpenAI from "openai";
-import { PrismaClient, User, Post } from '@prisma/client'
+import { PrismaClient, User, Post } from "@prisma/client";
+import axios from "axios";
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 dotenv.config();
 
@@ -14,21 +16,79 @@ const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
+console.log(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, OPENAI_API_KEY);
+
 if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET || !OPENAI_API_KEY)
   throw new Error("Missing credentials");
 
-const spotifyApi = SpotifyApi.withClientCredentials(
-  SPOTIFY_CLIENT_ID,
-  SPOTIFY_CLIENT_SECRET
-);
+const spotifyRedirectUri = "http://localhost:3000/callback";
+
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 
 const server = fastify({ logger: true });
 
+server.register(cors, {
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "OPTIONS"],
+});
+server.register(cookie, {});
+server.register(oauthPlugin, {
+  name: "spotifyOAuth2",
+  credentials: {
+    client: {
+      id: SPOTIFY_CLIENT_ID,
+      secret: SPOTIFY_CLIENT_SECRET,
+    },
+    auth: oauthPlugin.SPOTIFY_CONFIGURATION,
+  },
+  startRedirectPath: "/login",
+  callbackUri: spotifyRedirectUri,
+  scope: ["user-read-private", "user-read-email"],
+});
+
 server.get("/ping", async (request, reply) => {
   return { hello: "world" };
+});
+
+server.get<{
+  Querystring: {
+    code?: string;
+  };
+}>("/callback", async (request, reply) => {
+  const { code } = request.query;
+
+  const res = await axios.post(
+    "https://accounts.spotify.com/api/token",
+    {
+      code,
+      redirect_uri: spotifyRedirectUri,
+      grant_type: "authorization_code",
+    },
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${Buffer.from(
+          `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+        ).toString("base64")}`,
+      },
+    }
+  );
+  console.log(res.data);
+  if (res.status !== 200) {
+    reply.redirect("/invalid_token");
+  }
+  const { access_token, token_type, expires_in, refresh_token } = res.data;
+  const token: AccessToken = {
+    access_token,
+    refresh_token,
+    expires_in,
+    token_type,
+  };
+  const spotifyApi = SpotifyApi.withAccessToken(SPOTIFY_CLIENT_ID, token);
+  const user = await spotifyApi.currentUser.profile();
+  reply.send(user);
 });
 
 server.post<{ Body: { foo: string } }>("/test", async (request, reply) => {
@@ -56,13 +116,11 @@ server.post<{ Body: { foo: string } }>("/test", async (request, reply) => {
 });
 
 const start = async () => {
-  const userFound = await prisma.user.findUnique({
-
-    where:{ 
-        email: 'sdfhs'
-    }
-  
-  })
+  // const userFound = await prisma.user.findUnique({
+  //   where: {
+  //     email: "sdfhs",
+  //   },
+  // });
   // const user = await prisma.user.create({
   //   data: {
   //     email: 'sdfhs',
@@ -74,14 +132,9 @@ const start = async () => {
   //   }
   // })
   // console.log(user)
-  console.log(userFound)
+  // console.log(userFound);
 
   try {
-    await server.register(cors, {
-      origin: ["http://localhost:5173"],
-      methods: ["GET", "POST", "OPTIONS"],
-    });
-
     await server.listen({ port: 3000 });
 
     const address = server.server.address();
